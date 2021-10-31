1d112fca9ea36c8755d3a32560bb83afac1bebc7
SebastianMakila <info@kaikkitietokoneista.net>
2021-10-31 20:51:01 +0200

    Global variables

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.locals = {
+  site: {
+      title: 'PowerPaste',
+      description: 'The best pastebin service ever'
+  }
+}
+
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
