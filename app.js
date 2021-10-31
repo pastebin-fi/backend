86e147a7df3b2e46ab857430174cc45e50152ef9
SebastianMakila <info@kaikkitietokoneista.net>
2021-10-31 20:41:47 +0200

    Made simple page

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render('pages/index')
 })
 
 app.listen(port, () => {
