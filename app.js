29b84030ae85e1f9cb840855d3688b49c4a384c8
SebastianMakila <info@kaikkitietokoneista.net>
2021-11-05 19:12:10 +0200

    Use protocol in sitemap and in logs

--- app.js
+++ app.js
@@ -76,7 +76,9 @@ app.locals = {
   site: {
       title: process.env.TITLE,
       description: process.env.DESCRIPTION,
-      hostname: hostname
+      protocol,
+      hostname,
+      port,
   },
   defaultPaste: {
     content: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta tempore ad amet accusamus mollitia quis culpa provident odio facere, dolor quibusdam deleniti fuga minus vero molestias asperiores sequi! Officia atque, hic aspernatur culpa necessitatibus cumque doloremque rem. Fugiat vitae consectetur dolore eos voluptatibus vel, laborum saepe repellendus, dignissimos quaerat aut minus suscipit omnis possimus ipsam cumque sint repellat doloribus quasi neque quos laboriosam temporibus ullam? Ipsa maiores sequi quod perspiciatis vero cumque voluptatum quibusdam, ex impedit necessitatibus! Aliquid nulla ipsam, cupiditate aspernatur id eius fugit quasi maxime esse nam cum. Sunt tempore exercitationem praesentium, recusandae omnis asperiores sequi mollitia amet!
@@ -206,5 +208,5 @@ app.get('/sitemap.xml', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`PowerPaste app listening at http://${hostname}:${port}`)
+  console.log(`PowerPaste app listening at ${protocol}://${hostname}:${port}`)
 })
