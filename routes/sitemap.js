c218756cede526b766a6767f49f03b72d4139c91
RoyTakanen <roy@imap.fi>
2022-02-04 09:17:23 +0200

    do not show hidden pastes in sitemap

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -1,7 +1,7 @@
 const Paste = require('./paste').Paste;
 
 exports.get = (req, res) => {
-    Paste.find().exec((err, pastes) => {
+    Paste.find({ hidden: false }).exec((err, pastes) => {
         if (err) throw err
         res.set('Content-Type', 'text/xml');
         res.render('sitemap', {
