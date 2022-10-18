99f1d07cde96e79e3117bd8b4f95d3632d3cc96c
toke <toke8677@gmail.com>
2022-10-18 16:19:36 +0300

    cleanup user ratelimiter

--- src/ratelimiters/users.js
+++ src/ratelimiters/users.js
@@ -1,18 +1,19 @@
+// Limit each IP to 5 create account requests per `window` (here, per day)
+// -> keep in mind that account creations can be unsuccesful but they should 
+//    not because frontend tells if cannot be created
 export const createAccountLimiter = rateLimit({
 	windowMs: 60 * 60 * 1000 * 24, // 1 day
-	max: 5, // Limit each IP to 5 create account requests per `window` (here, per day) -> keep in mind that account creations can be unsuccesful but they should not because frontend tells if cannot be created
-    message: {
-        message: 'Too many accounts created from this IP.',
-    },
+	max: 5,
+    message: "Too many accounts have been created from this IP.",
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
 
+// Limit each IP to 20 login requests per `window` (here, 30 mins)
 export const loginAccountLimiter = rateLimit({
-	windowMs: 30 * 60 * 1000 , // 30 minutes
-	max: 20, // Limit each IP to 20 login requests per `window` (here, 30 mins)
-	message:
-		'Too many login attempts for this IP.',
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	windowMs: 30 * 60 * 1000 ,
+	max: 20,
+	message: "Too many login attempts have been done by this IP.",
+	standardHeaders: true, 
+	legacyHeaders: false, 
 });
