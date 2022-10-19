2728c063e1e3661294c152ad5b87f9cd95f38942
toke <toke8677@gmail.com>
2022-10-19 21:24:01 +0300

    Implement logger as class

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,27 @@
-const logger = {}
+class Logger {
+    constructor(useColors, exitOnError) {
+        this.useColors = useColors
+        this.exitOnError = exitOnError
+    }
 
-logger.log = function(...args) {
-    console.log("[log]", args)
+    log(...args) {
+        this.#printLine("log", 32, args)
+    }
+
+    warn(...args) {
+        this.#printLine("warn", 33, args)
+    }
+
+    error(...args) {
+        this.#printLine("error", 31, args)
+        if (this.exitOnError) process.exit(1)
+    }
+
+    #printLine(type, color, ...args) {
+        if (this.useColors) {
+            console.log(`(\x1b[${color}m${type}\x1b[0m)`, args.join(" "))
+        } else console.log(`(${type})`, args.join(" "))
+    }
 }
 
-return logger
+export { Logger }
