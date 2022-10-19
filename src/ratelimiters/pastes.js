d8f2a3cb870ede9f072ad678d0d9c60dfe681bc3
toke <toke8677@gmail.com>
2022-10-19 21:24:33 +0300

    Improve ratelimiter message

--- src/ratelimiters/pastes.js
+++ src/ratelimiters/pastes.js
@@ -1,12 +1,11 @@
+import rateLimit from "express-rate-limit"
 
-import rateLimit from 'express-rate-limit';
-
+// Limit each IP to 20 new paste requests per `window` (here, 30 mins)
 export const newPasteRateLimiter = rateLimit({
-	windowMs: 30 * 60 * 1000 , // 30 minutes
-	max: 20, // Limit each IP to 20 new paste requests per `window` (here, 30 mins)
-	message: {
-        message: 'Too many pastes created from this IP.'
-    },
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
+    windowMs: 30 * 60 * 1000,
+    max: 20,
+    message:
+        "Liian monta liitettä on luotu IP-osoitteestasi. Kokeile myöhemmin uudelleen.",
+    standardHeaders: true,
+    legacyHeaders: false,
+})
